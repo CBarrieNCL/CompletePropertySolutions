79d7b84b11d6a1e779f779ce4b230f590f66773e
callum.barrie <s2262480@students.ncl-coll.ac.uk>
2024-05-12 22:47:12 +0100

    minor additions, attempt at creating a slideshow

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 def main():
     st.markdown("""<h1 style="color: #FE7A36; font: "sans serif";>Welcome to Complete Property Solutions</h1>""", unsafe_allow_html=True)
@@ -6,16 +7,44 @@ def main():
 
     st.markdown("---")
 
-    st.markdown("""<h1 style="color: #FE7A36; font: "sans serif";>Services</h1>""", unsafe_allow_html=True)
-    st.write("""
-    - Property Management
-    - Real Estate Sales and Purchases
-    - Property Renovations and Repairs
-    - Rental Services
-    - Property Consultation
-    - Legal Assistance
-    - Investment Opportunities
-    """)
+    # Create columns for Services and Slideshow
+    col1, col2 = st.columns(2)
+
+    # Services section
+    with col1:
+        st.markdown("""<h1 style="color: #FE7A36; font: "sans serif";>Services</h1>""", unsafe_allow_html=True)
+        st.write("""
+        - Property Management
+        - Real Estate Sales and Purchases
+        - Property Renovations and Repairs
+        - Rental Services
+        - Property Consultation
+        - Legal Assistance
+        - Investment Opportunities
+        """)
+
+    # Slideshow section
+    with col2:
+        current_image = 0  # Initialize a counter for the current image index
+        images = ["https://purepng.com/public/uploads/large/purepng.com-househousebuildinghomewood-houseconcrete-house-1701528486802h5244.png",
+                  "https://purepng.com/public/uploads/large/purepng.com-small-househousebuildinghomewood-houseconcrete-house-1701528486721nysoi.png"
+                  ]
+
+
+    while True:
+        # Assign a unique key for each image display
+        image_key = f"slideshow_image_{current_image}"
+        st.image(images[current_image], width=300, caption=f"Image {current_image+1}", use_column_width=True, key=image_key)
+
+        # Update image index and handle wrapping
+        current_image = (current_image + 1) % len(images)
+
+        time.sleep(2)  # Optional pause for image transition
+
+        # Break out of the loop if the stop button is clicked
+        if st.button("Stop Slideshow"):
+            break
+
 
     st.markdown("---")
 
